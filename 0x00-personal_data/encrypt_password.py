ffb1e2c6cf3d42f5a76a3260d8701e454655610a
Keamogetsw3 <Keamogetswemaswanganyi@gmail.com>
2024-11-06 13:33:29 +0000

    Implement a hash_password function that expects one string argument name password and returns a salted, hashed password, which is a byte string.

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""""
+hash_password function that expects one string argument name password and returns a salted, hashed passwor
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    return hashed_password
