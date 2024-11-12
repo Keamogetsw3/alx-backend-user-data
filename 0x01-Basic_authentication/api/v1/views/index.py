8f6eeb044b7cba0e0cecbd0e84a3e3182a485796
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2024-11-12 11:17:40 +0200

    For testing this new error handler, add a new endpoint in api/v1/views/index.py:  Route: GET /api/v1/unauthorized This endpoint must raise a 401 error by using abort - Custom Error Pages By calling abort(401), the error handler for 401 will be executed.

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def _unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Raises:
+      - 401 Unauthorized error
+    """
+    abort(401)
