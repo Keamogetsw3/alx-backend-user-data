cbc2a639e211f1ce6134e704958a7412603b17c6
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2024-11-12 12:18:14 +0200

    Create the class Auth: in the file api/v1/auth/auth.py import request from flask class name Auth public method def require_auth(self, path: str, excluded_paths: List[str]) -> bool: that returns False - path and excluded_paths will be used later, now, you don’t need to take care of them public method def authorization_header(self, request=None) -> str: that returns None - request will be the Flask request object public method def current_user(self, request=None) -> TypeVar('User'): that returns None - request will be the Flask request object This class is the template for all authentication system you will implement.

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Module manage the API authentication
+"""
+from flask import jsonify, request
+from typing import List, TypeVar
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Check if authentication is required for the given path.
+        Returns: False
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Return the authorization header from the Flask request object.
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Return the current user
+        """
+        return None
