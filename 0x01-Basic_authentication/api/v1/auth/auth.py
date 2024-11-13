8865a441e6f122f4e853704f79778255701e0a48
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2024-11-14 00:27:43 +0200

    Update the method def authorization_header(self, request=None) -> str: in api/v1/auth/auth.py:  If request is None, returns None If request doesn’t contain the header key Authorization, returns None Otherwise, return the value of the header request Authorization

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -50,7 +50,9 @@ class Auth:
         Returns:
             str: The authorization header if present, None otherwise.
         """
-        return None
+        if request is None:
+            return None
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
